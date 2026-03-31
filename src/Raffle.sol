5168f9f44a86ede54157f59155cf63dcf638698b
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-31 10:07:19 +0200

    [update] - Contract with inheritance

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus {
+    /* Errors */
+    error Raffle__NotEnoughEthSent();
+
+    /* Type declarations */
+    enum State {
+        Open,
+        Calculating
+    }
+
+    /* State variables */
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subId;
+    uint16 private constant CONFIRMATIONS = 3;
+    uint32 private constant GAS_LIMIT = 500000;
+    uint32 private constant NUM_WORDS = 1;
+
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+
+    /* Events */
+    event NewPlayer(address indexed sender);
+    event WinnerRequested(address indexed sender);
+    event NewWinner(address indexed winner);
+
+    /* Constructor */
+    constructor(address vrfCoordinator, bytes32 keyHash) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_keyHash = keyHash;
+    }
+
+    /* Functions */
+    function enterRaffle() external payable {}
+
+    function fulfillRandomWords(
+        uint256,
+        /* requestId */
+        uint256[] calldata randomWords
+    )
+        internal
+        override
+    {}
+
+    /* Getter functions */
+    function getPlayers() external view returns (address payable[] memory) {
+        return s_players;
+    }
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
